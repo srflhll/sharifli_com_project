e413483338915053e409088b75130e2e3fb42d0a
hilalsharifli <hilal@sharifli.com>
2023-02-24 14:25:27 +0400

    hh

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Blog
+from django.core.paginator import Paginator
 
 
 def all_blogs(request):
-    blogs = Blog.objects.order_by('-date')
+    blog_list = Blog.objects.all()
+    paginator = Paginator(blog_list, 10)  # Set the number of items per page
+    page = request.GET.get('page')
+    blogs = paginator.get_page(page)
+    # commented by hsh blogs = Blog.objects.order_by('-date')
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
 
 
