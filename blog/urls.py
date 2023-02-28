e435a10194cb7416f33275461a422aa9f38fefe5
hilalsharifli <hilal@sharifli.com>
2023-02-28 14:10:29 +0400

    hh

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'blog'
 
@@ -7,3 +9,6 @@ urlpatterns = [
     path('', views.all_blogs, name='all_blogs'),
     path('<int:blog_id>/', views.detail, name='detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
